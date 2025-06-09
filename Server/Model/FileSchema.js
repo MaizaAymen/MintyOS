1ea671ea34c06222f23fde881f42a5080a1f558f
MaizaAymen <aymenmaiza87@gmail.com>
2025-06-09 20:38:37 +0100

    make the FileSchema

---
+++ Server/Model/FileSchema.js
@@ -0,0 +1,40 @@
+const mongoose= require(('mongoose'));
+const fileSchema= new mongoose.fileSchema({
+    filename:{
+        type:String,
+        required:true
+    },
+    path:{
+        type:String,
+        required:true
+    },
+    parentPath:{
+        type:mongoose.Schema.types.objectId,
+        ref:"File"
+    },
+    isDirectory:{
+        type:Boolean,
+        default:false
+    },
+    createAt:{
+        type:Date,
+        default:Date.now
+    },
+    owner:{
+        type:mongoose.Schema.types.objectId,
+        ref:"User"
+    },
+    content:{
+        type:String,
+        default:"",
+    },
+    createAt:{
+        type:Date,
+        default:Date.now
+    },
+    updateAt:{
+        type:Date,
+        default:Date.now
+    }
+
+})
