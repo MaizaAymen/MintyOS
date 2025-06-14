a5110b681e9a2e165b273e6a2dcb5b5bd532087d
MaizaAymen <aymenmaiza87@gmail.com>
2025-06-14 23:55:41 +0100

    i make the routes

---
+++ Server/Routes/Fileroutes.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router=express.Router();
+const fileController=require('../Controller/FileController');
+
+
+router.post('/create',fileController.createFile);
+
+router.delete('/delete',fileController.deleteFile);
+
+router.get('list',fileController.listeFiles);
+
+router.put('/update',fileController.updateFile);
